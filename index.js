623c4010f7d51f71376213eb51531b4228a5aa1a
billytrinh <quanghoa.trinh@gmail.com>
2022-10-31 21:15:50 +0700

    connect mysql

--- index.js
+++ index.js
@@ -5,82 +5,88 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,function () {
    console.log("Server is running...");
 });
+// config to connect mysql
+const configDB = {
+  host: "localhost",
+  port: 3306,
+  database: "T2207E",
+  user: "root",
+  password:"root", // mamp : "root" -- xampp: ""
+  multipleStatements: true // cho phép sử dụng nhiều câu SQL 1 lần gửi yêu cầu
+};
+// connect to mysql
+const mysql = require("mysql");
+const conn = mysql.createConnection(configDB);
 
-var ls = [
-    {
-        id: 1,
-        name:"Nguyễn Hoàng Nam",
-        age: 18
-    },
-    {
-        id:2,
-        name: "Thái Sơn",
-        age: 16
-    }
-];
-
-app.get("/demo",function (req,res) {
-   res.send("Hello world!");
+// api list all class
+app.get("/get-classes",function (req,res) {
+    const sql = "select * from classes";
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.send("404 not found");
+        }else{
+            res.send(data);
+        }
+    })
 });
-
-app.get("/get-data",function (req,res) {
-    res.send(ls);
+// get all students
+app.get("/get-students",function (req,res) {
+    const sql = "select * from students";
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.send("404 not found");
+        }else{
+            res.send(data);
+        }
+    })
 });
-app.get("/detail",function (req,res) {
-    var paramId = req.query.id;
-    var data;
-    for(var i=0;i<ls.length;i++){
-        if(ls[i].id == paramId){
-            data = ls[i];
-            break;
+// loc theo cid
+app.get("/student-by-class",function (req,res) {
+    const cid = req.query.cid;
+    const sql = "select * from students where cid = "+cid;
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.send("404 not found");
+        }else{
+            res.send(data);
         }
-    }
-    res.send(data);
-})
-app.get("/edit",function (req,res) {
-    var paramId = req.query.id;
-    var paramName = req.query.name;
-    var paramAge = req.query.age;
-    for(var i=0;i<ls.length;i++){
-        if(ls[i].id == paramId){
-            ls[i].name = paramName;
-            ls[i].age = paramAge;
-            break;
+    })
+});
+//search by name
+app.get("/search-students",function (req,res) {
+    const q = req.query.q;
+    const sql = `select * from students where name like '%${q}%' or email like '%${q}%'`;
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.send("404 not found");
+        }else{
+            res.send(data);
         }
-    }
-    res.send("done");
-})
-
-app.get("/create",function (req,res) {
-    var paramId = req.query.id;
-    var paramName = req.query.name;
-    var paramAge = req.query.age;
-    var check = false;
-    for(var i=0;i<ls.length;i++){
-        if(ls[i].id == paramId){
-            check= true;
-            break;
+    })
+});
+// search by class name
+app.get("/search-by-classname",function (req,res) {
+    const q = req.query.q;
+    const sql = `select * from students where cid in (select cid from classes where name like '%${q}%')`;
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.send("404 not found");
+        }else{
+            res.send(data);
         }
-    }
-    if(check == false){
-        ls.push({
-            id: paramId,
-            name: paramName,
-            age: paramAge
-        })
-    }
-    res.send("done");
-})
-app.get("/delete",function (req,res) {
-    var paramId = req.query.id;
-    var p=-1;
-    for(var i=0;i<ls.length;i++){
-        if(ls[i].id == paramId){
-            p = i;
+    })
+});
+// get 1 student by sid
+app.get("/detail-student",function (req,res) {
+    const sid = req.query.sid;
+    const sql = `select * from students where sid= ${sid}`;
+    conn.query(sql,function (err,data) {
+        if(err){
+            res.status(403).send("Error");
+        }else if(data.length > 0){
+            res.send(data[0]);
+        }else{
+            res.status(404).send("404 not found");
         }
-    }
-    if(p != -1){
-        ls.splice(p,1);
-    }
-    res.send("done");
-})
+    })
+});
